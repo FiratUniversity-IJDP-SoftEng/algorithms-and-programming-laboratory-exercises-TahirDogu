093d88318cc3c3f5c5542ce7d2901585d2e6aa7d
Mehmet Tahir Dogu <tahirdogu3351@gmail.com>
2025-03-15 23:00:35 +0300

    ex1

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,3 +1,8 @@
 # Your Student ID:230543009
-# Your Name and Surname:
-print("hello")
+# Your Name and Surname:Mehmet Tahir Doğu
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are!") 
+print("\t\tUp above the world so high, ")
+print("\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are")
