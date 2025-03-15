71b93f06ede2ade1bce8212ec90b4561f21bd141
Mehmet Tahir Dogu <tahirdogu3351@gmail.com>
2025-03-15 23:22:41 +0300

    ex8

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,5 +1,5 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543009
+# Your Name and Surname:Mehmet Tahir Doğu
 number = int(input("Enter the number of rows: "))
 
 for i in range(1, number + 1):
