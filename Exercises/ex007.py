8b54ef56a4ede56c1f9d679a08d097102754f01e
Mehmet Tahir Dogu <tahirdogu3351@gmail.com>
2025-03-15 23:14:18 +0300

    ex7

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,7 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543009
+# Your Name and Surname:Mehmet Tahir Doğu
+word = input("Please enter a string: ")
+listofWord = set(word)
+print("Character frequencies:")
+for letter in sorted(listofWord):
+    print(f"{letter} : {word.count(letter)}")
