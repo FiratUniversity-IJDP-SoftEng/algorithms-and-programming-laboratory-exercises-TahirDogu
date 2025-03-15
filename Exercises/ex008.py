935591b9ddb53b601c42057fbd938cd83443a08f
Mehmet Tahir Dogu <tahirdogu3351@gmail.com>
2025-03-15 23:15:59 +0300

    ex8

--- Exercises/ex008.py
+++ Exercises/ex008.py
@@ -1,2 +1,8 @@
 # Your Student ID:
 # Your Name and Surname:
+number = int(input("Enter the number of rows: "))
+
+for i in range(1, number + 1):
+    spaces = ' ' * (number - i)
+    stars = '*' * (2 * i - 1)
+    print(spaces + stars)
