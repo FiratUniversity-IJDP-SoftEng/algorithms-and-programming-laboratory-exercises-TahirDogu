a642332178a55273703573be1a5e14175e89c914
Mehmet Tahir Dogu <tahirdogu3351@gmail.com>
2025-03-15 23:18:13 +0300

    ex9

--- Exercises/ex009.py
+++ Exercises/ex009.py
@@ -1,2 +1,15 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543009 
+# Your Name and Surname:Mehmet Tahir Doğu
+print("1: Celsius to Fahrenheit")
+print("2: Fahrenheit to Celsius")
+
+choice = input("Which conversion do you want to use (1 or 2): ")
+
+if choice == "1":
+    celsius = float(input("Enter Celsius value : "))
+    print(f"Sonuç: {(celsius * 9/5) + 32:.2f} Fahrenheit")
+elif choice == "2":
+    fahrenheit = float(input("Enter Fahrenheit value: "))
+    print(f"Sonuç: {(fahrenheit - 32) * 5/9:.2f} Celsius")
+else:
+    print("Please enter 1 or 2")
