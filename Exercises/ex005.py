a15eafa609e69676f46420e321003d33d00ddcef
Mehmet Tahir Dogu <tahirdogu3351@gmail.com>
2025-03-15 23:11:13 +0300

    ex5

--- Exercises/ex005.py
+++ Exercises/ex005.py
@@ -1,2 +1,8 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543009 
+# Your Name and Surname:Mehmet Tahir Doğutahir
+firstName = input("Please enter your first name: ")
+lastName = input("Please enter your last name: ")
+nameLength = len(firstName)
+hiLength = len("hi")
+print(f"Hi{nameLength * " "}{lastName}")
+print(f"{hiLength *" "}{firstName}")
